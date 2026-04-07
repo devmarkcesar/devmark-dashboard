3de0c3b130eed3afa88d2030e974365fc86690d7
cesaranayaplai <cesar.anaya@plai.mx>
2026-04-06 20:29:37 -0600

    fix: exclude /logos/ from auth middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,5 @@ export default withAuth({
 })
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|api/auth).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|logos|api/auth).*)'],
 }
